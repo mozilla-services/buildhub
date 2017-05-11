4c727069f46b2f23925ddaf6daa9090782421507
Mathieu Leplatre <mathieu@leplat.re>
2017-05-11 16:56:53 +0200

    Retry fetching and publishing records (#10)

--- jobs/scrape_archives.py
+++ jobs/scrape_archives.py
@@ -1,4 +1,5 @@
 import asyncio
+import async_timeout
 import concurrent.futures
 import json
 import logging
@@ -7,6 +8,7 @@ import sys
 from packaging.version import parse as version_parse
 
 import aiohttp
+import backoff
 import kinto_http.exceptions
 from kinto_http import cli_utils
 
@@ -16,20 +18,19 @@ PRODUCTS = ("firefox", "thunderbird")
 DEFAULT_SERVER = "https://kinto-ota.dev.mozaws.net/v1"
 DEFAULT_BUCKET = "build-hub"
 DEFAULT_COLLECTION = "archives"
-NB_THREADS = 5
+NB_THREADS = 3
+NB_RETRY_REQUEST = 3
+TIMEOUT_SECONDS = 5 * 60
 
 
 logger = logging.getLogger(__name__)
 
 
 def publish_records(client, records):
-    try:
-        with client.batch() as batch:
-            for record in records:
-                batch.create_record(record, safe=False)
-        logger.info("Created %s records" % len(records))
-    except kinto_http.exceptions.KintoException:
-        logger.exception("Could not create records")
+    with client.batch() as batch:
+        for record in records:
+            batch.create_record(record)
+    logger.info("Created %s records" % len(records))
 
 
 def archive_url(product, version=None, platform=None, locale=None):
@@ -43,17 +44,21 @@ def archive_url(product, version=None, platform=None, locale=None):
     return url
 
 
+@backoff.on_exception(backoff.expo,
+                      asyncio.TimeoutError,
+                      max_tries=NB_RETRY_REQUEST)
 async def fetch_listing(session, url):
     headers = {
         "Accept": "application/json",
         "User-Agent": "BuildHub;storage-team@mozilla.com"
     }
-    async with session.get(url, headers=headers) as response:
-        try:
-            data = await response.json()
-            return data["prefixes"], data["files"]
-        except (aiohttp.ClientError, KeyError) as e:
-            raise ValueError("Could not fetch %s: %s" % (url, e))
+    with async_timeout.timeout(TIMEOUT_SECONDS):
+        async with session.get(url, headers=headers, timeout=None) as response:
+            try:
+                data = await response.json()
+                return data["prefixes"], data["files"]
+            except (aiohttp.ClientError, KeyError) as e:
+                raise ValueError("Could not fetch %s: %s" % (url, e))
 
 
 async def fetch_products(session, queue, products):
@@ -79,7 +84,7 @@ async def fetch_platforms(session, queue, product, version):
     platform_folders, _ = await fetch_listing(session, version_url)
 
     platforms = [p[:-1] for p in platform_folders]  # strip trailing /
-    platforms = [p for p in platforms if p not in ("source", "update")]
+    platforms = [p for p in platforms if p not in ("source", "update", "contrib")]
 
     futures = [fetch_locales(session, queue, product, version, platform)
                for platform in platforms]
@@ -103,7 +108,7 @@ async def fetch_files(session, queue, product, version, platform, locale):
     _, files = await fetch_listing(session, locale_url)
 
     fileregexp = re.compile("%s-(.+)(zip|gz|bz|bz2|dmg|apk)$" % product)
-    files = [f for f in files if fileregexp.match(f["name"])]
+    files = [f for f in files if fileregexp.match(f["name"]) and 'sdk' not in f["name"]]
 
     futures = []
     for file_ in files:
@@ -141,24 +146,39 @@ async def produce(loop, queue):
     logger.info("Scraping releases done.")
 
 
-async def consume(loop, executor, client, queue):
+async def consume(loop, queue, executor, client):
     """Store grabbed releases from the archives website in Kinto."""
+
+    def markdone(queue, n):
+        """Returns a callback that will mark `n` queue items done."""
+        return lambda fut: [queue.task_done() for _ in range(n)]
+
     info = client.server_info()
     batch_size = info["settings"]["batch_max_requests"]
     while True:
-        records = [await queue.get()]
-        while not queue.empty() and len(records) < batch_size:
-            records.append(await queue.get())
-        task = loop.run_in_executor(executor, publish_records, client, records)
-        # Counting tasks done allows us to wait for the queue with queue.join()
-        task.add_done_callback(lambda fut: queue.task_done())
+        # Consume records from queue, and batch operations.
+        # But don't block if there's not enough records to fill a batch.
+        batch = []
+        try:
+            with async_timeout.timeout(2):
+                while len(batch) < batch_size:
+                    record = await queue.get()
+                    batch.append(record)
+        except asyncio.TimeoutError:
+            logger.debug("Scraping done or not fast enough, proceed.")
+            pass
+
+        if batch:
+            task = loop.run_in_executor(executor, publish_records, client, batch)
+            task.add_done_callback(markdone(queue, len(batch)))
 
 
-def main():
+async def main(loop):
     parser = cli_utils.add_parser_options(
         description="Send releases archives to Kinto",
         default_server=DEFAULT_SERVER,
         default_bucket=DEFAULT_BUCKET,
+        default_retry=NB_RETRY_REQUEST,
         default_collection=DEFAULT_COLLECTION)
 
     args = parser.parse_args(sys.argv[1:])
@@ -169,30 +189,26 @@ def main():
                 .format(**args.__dict__))
 
     client = cli_utils.create_client_from_args(args)
-    client.session.nb_retry = 3
 
     public_perms = {"read": ["system.Everyone"]}
     client.create_bucket(permissions=public_perms, if_not_exists=True)
     client.create_collection(if_not_exists=True)
 
-    loop = asyncio.get_event_loop()
-    queue = asyncio.Queue(loop=loop)
-
+    # Start a producer and a consumer with threaded kinto requests.
+    queue = asyncio.Queue()
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=NB_THREADS)
-    consumer_coro = consume(loop, executor, client, queue)
-    consumer_task = asyncio.ensure_future(consumer_coro)
-
-    # Wait for scraping of releases and all records to be published.
-    producer = produce(loop, queue)
-    wait_queue = queue.join()
-    future = asyncio.gather(producer, wait_queue)
-    try:
-        loop.run_until_complete(future)
-    finally:
-        # Everything processed, stop consuming.
-        consumer_task.cancel()
-        loop.close()
+    # Schedule the consumer
+    consumer_coro = consume(loop, queue, executor, client)
+    consumer = asyncio.ensure_future(consumer_coro)
+    # Run the producer and wait for completion
+    await produce(loop, queue)
+    # Wait until the consumer is done consuming everything.
+    await queue.join()
+    # The consumer is still awaiting for the producer, cancel it.
+    consumer.cancel()
 
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+    loop.close()
