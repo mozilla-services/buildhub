6ef5618d6c9726dc8d45b21300f345ea42e1d68f
Mathieu Leplatre <mathieu@leplat.re>
2017-05-15 11:54:57 +0200

    Fetch nightlies (#13)

--- jobs/scrape_archives.py
+++ jobs/scrape_archives.py
@@ -1,6 +1,7 @@
 import asyncio
 import async_timeout
 import concurrent.futures
+import datetime
 import json
 import logging
 import re
@@ -22,6 +23,7 @@ NB_THREADS = 3
 NB_RETRY_REQUEST = 3
 TIMEOUT_SECONDS = 5 * 60
 
+today = datetime.date.today()
 
 logger = logging.getLogger(__name__)
 
@@ -33,8 +35,37 @@ def publish_records(client, records):
     logger.info("Created %s records" % len(records))
 
 
-def archive_url(product, version=None, platform=None, locale=None):
-    url = ARCHIVE_URL + product + "/releases/"
+def archive(product, version, platform, locale, channel, url, size, date):
+    record = {
+        "build": None,
+        "source": {
+            "revision": None,
+            "tree": None,
+            "product": product,
+        },
+        "target": {
+            "platform": platform,
+            "locale": locale,
+            "version": version,
+            "channel": channel,
+        },
+        "download": {
+            "url": url,
+            "mimetype": None,
+            "size": size,
+            "date": date,
+        },
+        "systemaddons": None
+    }
+    return record
+
+
+def archive_url(product, version=None, platform=None, locale=None, nightly=None):
+    url = ARCHIVE_URL + product
+    if nightly:
+        url += "/nightly/" + nightly + "/"
+    else:
+        url += "/releases/"
     if version:
         url += version + "/"
     if platform:
@@ -61,17 +92,86 @@ async def fetch_listing(session, url):
                 raise ValueError("Could not fetch %s: %s" % (url, e))
 
 
-async def fetch_products(session, queue, products):
-    futures = [fetch_versions(session, queue, product) for product in products]
-    return await asyncio.gather(*futures)
+async def fetch_products(session, queue, products, client):
+    # Nightlies
+    futures = [fetch_nightlies(session, queue, product, client) for product in products]
+    await asyncio.gather(*futures)
+    # Releases
+    futures = [fetch_versions(session, queue, product, client) for product in products]
+    await asyncio.gather(*futures)
 
 
-async def fetch_versions(session, queue, product):
+async def fetch_nightlies(session, queue, product, client):
+    # Check latest known version on server.
+    filters = {
+        "source.product": product,
+        "target.channel": "nightly",
+        "_sort": "-download.date",
+        "_limit": 1,
+        "pages": 1
+    }
+    existing = client.get_records(**filters)
+    latest_nightly_folder = ""
+    if existing:
+        latest_nightly = existing[0]["download"]["date"]
+        nightly_datetime = datetime.datetime.strptime(latest_nightly, "%Y-%m-%dT%H:%M:%SZ")
+        latest_nightly_folder = nightly_datetime.strftime("%Y-%m-%d-%H-%M-%S")
+
+    current_month = "{}/{:02d}".format(today.year, today.month)
+    month_url = archive_url(product, nightly=current_month)
+    days_folders, _ = await fetch_listing(session, month_url)
+
+    # Skip aurora nightlies and known nightlies...
+    days_urls = [archive_url(product, nightly=current_month + "/" + f[:-1])
+                 for f in days_folders if f > latest_nightly_folder and f.endswith("mozilla-central/")]
+    futures = [fetch_listing(session, day_url) for day_url in days_urls]
+    listings = await asyncio.gather(*futures)
+
+    channel = "nightly"
+
+    for day_url, (_, files) in zip(days_urls, listings):
+        futures = []
+        for file_ in files:
+            filename = file_["name"]
+            size = file_["size"]
+            date = file_["last_modified"]
+            url = day_url + filename
+
+            match = re.search(r'\w+-(\d+.+)\.([a-z]+(\-[A-Z]+)?)\.(.+)\.tar.bz2+$', filename)
+            if not match:
+                continue
+            version = match.group(1)
+            locale = match.group(2)
+            platform = match.group(4)
+
+            record = archive(product, version, platform, locale, channel, url, size, date)
+            logger.debug("Nightly found %s" % record["download"]["url"])
+            futures.append(queue.put(record))
+        await asyncio.gather(*futures)
+
+
+async def fetch_versions(session, queue, product, client):
+    # Check latest known version on server.
+    filters = {
+        "source.product": product,
+        "target.channel": "beta",
+        "_sort": "-download.date",
+        "_limit": 1,
+        "pages": 1
+    }
+    existing = client.get_records(**filters)
+    latest_version = ""
+    if existing:
+        latest_version = existing[0]["target"]["version"]
+        logger.info("Scrape from version {}".format(latest_version))
+
     product_url = archive_url(product)
     versions_folders, _ = await fetch_listing(session, product_url)
 
     versions = [v[:-1] for v in versions_folders if re.match(r'^[0-9]', v)]
     versions = [v for v in versions if "funnelcake" not in v]
+    # Scrape only unknown recent versions.
+    versions = [v for v in versions if version_parse(v) > version_parse(latest_version)]
     versions = sorted(versions, key=lambda v: version_parse(v), reverse=True)
 
     futures = [fetch_platforms(session, queue, product, version)
@@ -110,39 +210,25 @@ async def fetch_files(session, queue, product, version, platform, locale):
     fileregexp = re.compile("%s-(.+)(zip|gz|bz|bz2|dmg|apk)$" % product)
     files = [f for f in files if fileregexp.match(f["name"]) and 'sdk' not in f["name"]]
 
+    channel = None  # Unknown.
+
     futures = []
     for file_ in files:
         filename = file_["name"]
-        record = {
-            "build": None,
-            "source": {
-                "revision": None,
-                "tree": None,
-                "product": product,
-            },
-            "target": {
-                "platform": platform,
-                "locale": locale,
-                "version": version,
-                "channel": None,
-            },
-            "download": {
-                "url": locale_url + filename,
-                "mimetype": None,
-                "size": None,
-            },
-            "systemaddons": None
-        }
+        url = locale_url + filename
+        size = file_["size"]
+        date = file_["last_modified"]
+        record = archive(product, version, platform, locale, channel, url, size, date)
         logger.debug("Release found %s" % record["download"]["url"])
         futures.append(queue.put(record))
 
     return await asyncio.gather(*futures)
 
 
-async def produce(loop, queue):
+async def produce(loop, queue, client):
     """Grab releases from the archives website."""
     async with aiohttp.ClientSession(loop=loop) as session:
-        await fetch_products(session, queue, PRODUCTS)
+        await fetch_products(session, queue, PRODUCTS, client)
     logger.info("Scraping releases done.")
 
 
@@ -201,7 +287,7 @@ async def main(loop):
     consumer_coro = consume(loop, queue, executor, client)
     consumer = asyncio.ensure_future(consumer_coro)
     # Run the producer and wait for completion
-    await produce(loop, queue)
+    await produce(loop, queue, client)
     # Wait until the consumer is done consuming everything.
     await queue.join()
     # The consumer is still awaiting for the producer, cancel it.
