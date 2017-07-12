fdb52c15b6f48f2bdb2798b212939569193b6ae7
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-07-12 12:14:05 +0200

    @natim review.

--- jobs/tests/test_utils.py
+++ jobs/tests/test_utils.py
@@ -170,7 +170,7 @@ RECORDS = [
 
     # Thunderbird Mac OS X Release
     {
-        "id": "thunderbird_10-0-12esr_macosx_pt-br",
+        "id": "thunderbird_esr_10-0-12esr_macosx_pt-br",
         "source": {
             "product": "thunderbird",
         },
@@ -178,7 +178,7 @@ RECORDS = [
             "version": "10.0.12esr",
             "platform": "macosx",
             "locale": "pt-BR",
-            "channel": "release"
+            "channel": "esr"
         },
         "download": {
             "url": "https://archive.mozilla.org/pub/thunderbird/releases/10.0.12esr/mac/pt-BR/"
@@ -188,7 +188,7 @@ RECORDS = [
 
     # Thunderbird Release
     {
-        "id": "thunderbird_17-0-8esr_linux-x86_64_gd",
+        "id": "thunderbird_esr_17-0-8esr_linux-x86_64_gd",
         "source": {
             "product": "thunderbird",
         },
@@ -196,7 +196,7 @@ RECORDS = [
             "version": "17.0.8esr",
             "platform": "linux-x86_64",
             "locale": "gd",
-            "channel": "release"
+            "channel": "esr"
         },
         "download": {
             "url": "https://archive.mozilla.org/pub/thunderbird/releases/17.0.8esr/linux-x86_64/"
