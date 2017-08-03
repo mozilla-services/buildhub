46ae4bf7a3339b12f58f6ac46e34288469fc74f5
Mathieu Leplatre <mathieu@mozilla.com>
2017-08-03 13:32:27 +0200

    Skip namoroka (fixes #192)

--- jobs/buildhub/utils.py
+++ jobs/buildhub/utils.py
@@ -121,7 +121,7 @@ def is_release_url(product, url):
         return False
 
     re_exclude = re.compile(".+(tinderbox|try-builds|partner-repacks|latest|contrib|/0\.|"
-                            "experimental|debug|sha1-installers|candidates|"
+                            "experimental|namoroka|debug|sha1-installers|candidates|"
                             "stylo-bindings|/1.0rc/|dominspector|/test/)")
     if re_exclude.match(url):
         return False
