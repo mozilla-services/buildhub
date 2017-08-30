4aa5c2295aa972cb6b095b7041c5b36ba1d2af81
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-08-30 15:07:42 +0200

    Use with buildhub stage.

--- ui/src/App.js
+++ ui/src/App.js
@@ -30,7 +30,7 @@ import {
 } from "searchkit";
 
 const searchkit = new SearchkitManager(
-  "https://kinto-ota.dev.mozaws.net/v1/buckets/build-hub/collections/releases/",
+  "https://buildhub.stage.mozaws.net/v1/buckets/buildhub/collections/releases/",
   { searchUrlPath: "search" }
 );
 
