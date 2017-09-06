770117e4801bf3a65f69692e366ddd6493b3914c
Mathieu Leplatre <mathieu@mozilla.com>
2017-09-06 16:21:34 +0200

    Preparing release 0.3.0

--- jobs/CHANGELOG.rst
+++ jobs/CHANGELOG.rst
@@ -3,7 +3,7 @@ Changelog
 
 This document describes changes between each past release.
 
-0.3.0 (unreleased)
+0.3.0 (2017-09-06)
 ------------------
 
 - Load ``initialization.yml`` from the S3 inventory lambda (#236)
--- jobs/setup.py
+++ jobs/setup.py
@@ -34,7 +34,7 @@ ENTRY_POINTS = {
 
 
 setup(name='buildhub',
-      version='0.3.0dev0',
+      version='0.3.0',
       description='Buildhub Python libraries.',
       long_description="{}\n\n{}\n\n{}".format(README, CHANGELOG, CONTRIBUTORS),
       license='Apache License (2.0)',
