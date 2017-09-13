a18cc825db919daf673d5ca5644266b6b424fafb
Mathieu Leplatre <mathieu@mozilla.com>
2017-09-13 20:21:03 +0200

    @Natim review

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM python:3.6-slim
 MAINTAINER Product Delivery irc://irc.mozilla.org/#product-delivery
 
-ENV KINTO_INI /etc/kinto/kinto.ini
-ENV PORT 8888
-
-
 COPY . /app
 WORKDIR /app
 
@@ -14,9 +10,7 @@ RUN \
     rm -rf /var/lib/apt/lists/* && \
     pip install virtualenv && \
     make virtualenv && \
-    .venv/bin/pip install jobs/ && \
-    .venv/bin/pip install kinto && \
-    .venv/bin/kinto init --ini $KINTO_INI --backend memory
+    .venv/bin/pip install jobs/
 
 RUN groupadd -g 10001 app && \
     useradd -M -u 10001 -g 10001 -G app -d /app -s /sbin/nologin app
--- bin/run.sh
+++ bin/run.sh
@@ -2,36 +2,39 @@
 set -eo pipefail
 
 usage() {
-  echo "usage: ./bin/run.sh version|lambda.zip|kinto|initialize-kinto|latest-inventory-to-kinto"
+  echo "usage: ./bin/run.sh version|test|lambda.zip|kinto|initialize-kinto|latest-inventory-to-kinto"
   echo ""
-  echo "    version                     show current version"
-  echo "    lambda.zip                  build Zip archive from buildhub package"
-  echo "    kinto                       run a Kinto server"
-  echo "    initialize-kinto            initialize a Kinto server for buildhub"
-  echo "    latest-inventory-to-kinto   load latest S3 inventory to a Kinto server"
+  echo "    version                     Show current version"
+  echo "    test                        Run tests"
+  echo "    lambda.zip                  Build Zip archive from buildhub package"
+  echo "    initialize-kinto            Initialize a Kinto server for buildhub"
+  echo "    latest-inventory-to-kinto   Load latest S3 inventory to a Kinto server"
   echo ""
   exit 1
 }
 
 [ $# -lt 1 ] && usage
 
-source .venv/bin/activate
-
 case $1 in
   version)
     cat version.json
+    ;;
+  test)
+    python3 -m venv /tmp/tests
+    /tmp/tests/bin/pip install jobs/
+    /tmp/tests/bin/pip install -r jobs/dev-requirements.txt
+    /tmp/tests/bin/py.test --ignore=jobs/tests/test_lamdba_s3_event_functional.py --override-ini="cache_dir=/tmp/tests" jobs/tests
     ;;
   lambda.zip)
     cd .venv/lib/python3.6/site-packages/
     zip -r ../../../../lambda.zip *
     ;;
-  kinto)
-    kinto start --ini $KINTO_INI --port $PORT
-    ;;
   initialize-kinto)
+    source .venv/bin/activate
     kinto-wizard load jobs/buildhub/initialization.yml ${@:2}
     ;;
   latest-inventory-to-kinto)
+    source .venv/bin/activate
     latest-inventory-to-kinto ${@:2}
     ;;
   *)
